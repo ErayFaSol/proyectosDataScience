d5aa6599f13b099983fba32508b82b507f5829ee
Eduardo Franco <eduardo.fsolorzano@alumnos.udg.mx>
2024-01-03 23:54:56 -0600

    Proyecto 5

---
+++ proyecto5/app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('proyecto5/vehicles_us.csv') # leer los datos
+encabezado = st.header('Proyecto sprint 5: Herramientas de desarrollo de software')
+hist_button = st.checkbox('Construir histograma') # crear un botón
+disp_button = st.checkbox('Construir diagrama de dispersion')
+
+
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:
+    st.write('Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True )
